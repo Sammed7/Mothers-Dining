c7126667b5f27735580d2a42942fa866dca8cbe8
Sammed Dadape <samdadape@gmail.com>
2023-12-28 11:55:24 +0530

    formatted code in all files

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const router = express.Router()
-const { signUp, getUsers, logIn } = require('../controllers/userController')
+const express = require("express");
+const router = express.Router();
+const { signUp, getUsers, logIn } = require("../controllers/userController");
 
-router.post('/signUp', signUp)
-router.post('/logIn', logIn)
-router.get('/getusers', getUsers)
+router.post("/signUp", signUp);
+router.post("/logIn", logIn);
+router.get("/getusers", getUsers);
 
-
-module.exports = router
+module.exports = router;
