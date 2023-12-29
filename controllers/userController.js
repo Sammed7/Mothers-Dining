64321626e61c380c47bc31ead80e23cd694ec1e1
Vaishnavi <mhetrevaishnavi23@gmail.com>
2023-12-29 12:28:01 +0530

    LogIn API with authentication
    Development of LogIn API with authentication and code formatting.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,104 +1,112 @@
-const User = require('../models/userModel')
-const asyncHandler = require('express-async-handler')
-const bcrypt = require('bcrypt')
+const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-// signUp user and save the details 
-const signUp = asyncHandler(async (req,res) => {
+// signUp user and save the details
+const signUp = asyncHandler(async (req, res) => {
+  const { name, email, password, confirm_password } = req.body;
 
-    const {name, email, password, confirm_password} = req.body
+  if (!name || !email || !password || !confirm_password) {
+    res.status(400).json({
+      message: "please enter all the required fields.",
+    });
+  }
 
-    if(!name || !email || !password || !confirm_password){
-       res.status(400).json({
-        message : "please enter all the required fields."
-       }) 
-    }
+  // check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400).json({
+      status: "failed",
+      message: "User already exists.",
+    });
+  }
 
-    // check if user already exists
-    const userExists = await User.findOne({email})
-    if(userExists) {
-        res.status(400).json({
-            status : "failed",
-            message: "User already exists."
-        })
-    }
+  // check if password amd confirm password matches
+  if (password !== confirm_password) {
+    res.status(400).json({
+      status: "failed",
+      message: "Password and confirm password dosen't match.",
+    });
+  }
 
-    // check if password amd confirm password matches
-    if(password !== confirm_password) {
-        res.status(400).json({
-            status: "failed",
-            message: "Password and confirm password dosen't match."
-        })
-    }
+  // hash the password with 10 digit
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    // hash the password with 10 digit
-    const hashedPassword = await bcrypt.hash(password , 10)
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
 
-    // create user 
-    const user = await User.create({
-        name,
-        email,
-        password : hashedPassword
-    })
-
-    if(user){
-        res.status(200).json({
-            status: "success",
-            _id : user.id,
-            name: user.name,
-            email: user.email,
-            password: user.password
-        })
-    }
-    else {
-        res.status(400).json({
-            statue: "Failed",
-            message: "Invalid user data."
-        })
-    }
-})
-
-//log in user 
-const logIn =  asyncHandler(async(req,res) => {
-    const {email, password} = req.body
-
-    if(!email || !password){
-        res.status(400).json({
-         message : "please enter all the required fields."
-        }) 
-     }
-
-    const user = await User.findOne({email})
-    if(!user){
-        res.status(400).json({
-            message : "User not found"
-        }) 
-    }
-    const DBpassword = await user.password
-
-    if(user && (await bcrypt.compare(password, DBpassword))){
-        res.status(200).json({
-            Status : "success",
-            name: user.name,
-            email: user.email,
-        })
-    }
-    else{
-        res.status(400).json({
-            Status : "Failed",
-            message: "Invalid credentials"
-        })
-    }
-})
-
-const getUsers =  asyncHandler(async(req,res) => {
-    const users = await User.find()
+  if (user) {
     res.status(200).json({
-        users
-    })
-})
+      status: "success",
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    });
+  } else {
+    res.status(400).json({
+      statue: "Failed",
+      message: "Invalid user data.",
+    });
+  }
+});
+
+//log in user
+const logIn = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({
+      message: "please enter all the required fields.",
+    });
+  }
+
+  const user = await User.findOne({ email });
+  const Token = genrateToken(user._id);
+
+  if (!user) {
+    res.status(400).json({
+      message: "User not found",
+    });
+  }
+  const DBpassword = await user.password;
+
+  if (user && (await bcrypt.compare(password, DBpassword))) {
+    res.status(200).json({
+      Status: "success",
+      name: user.name,
+      email: user.email,
+      token: Token,
+    });
+  } else {
+    res.status(400).json({
+      Status: "Failed",
+      message: "Invalid credentials",
+    });
+  }
+});
+
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find();
+  res.status(200).json({
+    users,
+  });
+});
+
+//Genrate JWT
+const genrateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "1m",
+  });
+};
 
 module.exports = {
-    signUp,
-    getUsers,
-    logIn
-}
+  signUp,
+  getUsers,
+  logIn,
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const router = express.Router()
-const { signUp, getUsers, logIn } = require('../controllers/userController')
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
+const { signUp, getUsers, logIn } = require("../controllers/userController");
 
-router.post('/signUp', signUp)
-router.post('/logIn', logIn)
-router.get('/getusers', getUsers)
+router.post("/signUp", signUp);
+router.post("/logIn", logIn);
+router.get("/getusers", protect, getUsers);
 
-
-module.exports = router
+module.exports = router;
